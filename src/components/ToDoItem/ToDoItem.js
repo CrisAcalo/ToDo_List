e8f05ffb4d9d42deacfe6868c97d41663ef817cb
CrisAcalo <cristianchoacalo@gmail.com>
2024-09-20 21:22:19 -0500

    Cambio de BootsrapIcons por React-icons

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -1,36 +1,35 @@
 import './ToDoItem.css';
 import React from 'react';
+import { BsCheck2 } from "react-icons/bs";
+import { BsBan } from "react-icons/bs";
+import { BsTrashFill } from "react-icons/bs";
 
 function ToDoItem(props) {
 
-    const [classIcon, setClassIcon] = React.useState(props.completed ? 'bi bi-check2 todoActionButton' : 'bi bi-ban todoActionButton');
-    const [styleIcon, setStyleIcon] = React.useState(props.completed ? { color: '#99ff99' } : { color: 'red' });
+    const [classIcon, setClassIcon] = React.useState(props.completed ? <BsCheck2 color='#99ff99' size={30} /> : <BsBan color='red' size={25} />);
 
     let styleText = props.completed ? { textDecoration: 'line-through', color: 'var(--secondary-color-light)' } : { textDecoration: 'none' };
 
     return (
         <li>
-            <i className={classIcon} style={styleIcon}
+            <div className="todoActionButton"
                 onMouseOver={
                     () => {
-                        setClassIcon(props.completed ? 'bi bi-ban todoActionButton' : 'bi bi-check2 todoActionButton');
-                        setStyleIcon(props.completed ? { color: 'red' } : { color: '#99ff99' });
+                        setClassIcon(props.completed ? <BsBan color='red' size={25} /> : <BsCheck2 color='#99ff99' size={30} />);
                     }
                 }
                 onMouseOut={
                     () => {
-                        setClassIcon(props.completed ? 'bi bi-check2 todoActionButton' : 'bi bi-ban todoActionButton');
-                        setStyleIcon(props.completed ? { color: '#99ff99' } : { color: 'red' });
+                        setClassIcon(props.completed ? <BsCheck2 color='#99ff99' size={30} /> : <BsBan color='red' size={25} />);
                     }
                 }
                 onClick={props.onComplete}
-            ></i>
-
+            >
+                {classIcon}
+            </div>
             <p style={styleText}>{props.text}</p>
             <div className="deleteIconContainer">
-                <i className="bi bi-trash-fill"
-                    onClick={props.onDelete}
-                ></i>
+                <BsTrashFill onClick={props.onDelete}/>
             </div>
         </li>
     );
